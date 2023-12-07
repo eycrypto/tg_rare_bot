265f1dd2fc39035a0830cac903dffb3f5c55a5d6
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2023-12-07 23:47:05 +0300

    Update admin.py

--- Models/admin.py
+++ Models/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from Models.models import Message, API, System, Rare
+from Models.models import Message, API, System, Rare, SendMessage 
 
 
 @admin.register(Message)
